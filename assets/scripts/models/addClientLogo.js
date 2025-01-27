d0c921541ee8bafce3d4a7685279a5a7af33b109
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2025-01-26 22:01:39 -0300

    quase finalizado

--- assets/scripts/models/addClientLogo.js
+++ assets/scripts/models/addClientLogo.js
@@ -1,23 +1,24 @@
 function addClientLogo (event, studioBrinImg){
   if ( event.files.length > 0 ) {
     const type = event.files[0].type;
-    const validationFormat = ['image/png', 'image/svg']
+    const validationFormat = ['image/png', 'image/svg+xml'];
 
     if(!validationFormat.includes(type)){
       alert('Formato não permitido, porfavor enviar somente .png ou .svg');
       return;
     }
 
+    if(document.querySelector('#logoClient')){
+      studioBrinImg.removeChild(document.querySelector('#logoClient'))
+    }
+
     const logoClient = document.createElement('img');
     logoClient.id = "logoClient";
     logoClient.src = URL.createObjectURL(event.files[0]);
-    logoClient.classList.add('studioBrin__client-logo')
+    logoClient.classList.add('studioBrin__client-logo');
+    logoClient.setAttribute('alt', 'Logo da empresa');
 
-
-    studioBrinImg.appendChild(logoClient)
-
-    console.log(studioBrinImg);
-
+    studioBrinImg.appendChild(logoClient);
   }
 }
 
